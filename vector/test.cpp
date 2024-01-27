22037b946185e6177e392d577ba465a947b6b3c9
MindbniM <118365358+MindbniM@users.noreply.github.com>
2024-01-27 16:35:08 +0800

    Add files via upload

--- vector/test.cpp
+++ vector/test.cpp
@@ -1,8 +1,21 @@
-#include"str.h"
+#include<iostream>
+#include<string>
+#include<vector>
+#include<assert.h>
+#include<algorithm>
+#include<list>
+#include"vector.h"
 int main()
 {
-	M::string s1("12345");
-	M::string s2 = s1.substr(0,2);
-	cout << s2<<endl;
+	std::list<char> lc;
+	lc.push_back('a');
+	lc.push_back('b');
+	lc.push_back('c');
+	lc.push_back('d');
+	M::vector<char> v(lc.begin(), lc.end());
+	for (auto& e : v)
+	{
+		std::cout << e << " ";
+	}
 	return 0;
 }
--- vector/vector.h
+++ vector/vector.h
@@ -9,20 +9,205 @@ namespace M
 	public:
 		typedef T* iterator;
 		typedef const T* const_iterator;
-		vector();
+		vector()
+		{}
+		vector(const vector<T>& v)
+		{
+			reserve(v.capacity());
+			for (auto& e : v)
+			{
+				push_back(e);
+			}
+		}
+		template<class ITERATOR>
+		vector(ITERATOR begin, ITERATOR end)
+		{
+			while (begin != end)
+			{
+				push_back(*begin);
+				begin++;
+			}
+		}
+		~vector()
+		{
+			delete[] _start;
+			_start = _finish = _end = nullptr;
+		}
+		void push_back(const T& val);
+		void pop_back();
+		void insert(iterator pos,const T& val);
+		iterator erase(iterator pos)
+		{
+			assert(size());
+			assert(_start <= pos);
+			assert(_finish >= pos);
+			iterator end = pos;
+			int n = pos - _start;
+			while (end < _finish)
+			{
+				*end = *(end + 1);
+				end++;
+			}
+			--_finish;
+			return _start + n;
+		}
+		void swap(vector<T>& v)
+		{
+			std::swap(_start, v._start);
+			std::swap(_finish, v._finish);
+			std::swap(_end, v._end);
+		}
+		T& front()
+		{
+			assert(_start);
+			return *_start;
+		}
+		const T& front() const
+		{
+			assert(_start);
+			return *_start;
+		}
+		T& back() 
+		{
+			assert(_start);
+			return *(_start+size()-1);
+		}
+		const T& back() const
+		{
+			assert(_start);
+			return *(_start + size()-1);
+		}
+		size_t capacity() const
+		{
+			return _end - _start;
+		}
+		size_t size() const
+		{
+			return _finish - _start;
+		}
+		void reserve(const size_t n);
+		void resize(const size_t n, T val = T());
+		iterator begin()
+		{
+			return _start;
+		}
+		iterator end()
+		{
+			return _finish;
+		}
+		const_iterator begin() const
+		{
+			return _start;
+		}
+		const_iterator end() const
+		{
+			return _finish;
+		}
+		T& operator[](size_t pos);
+		const T& operator[](size_t pos) const;
+		vector<T>& operator=(vector<T> v);
 	private:
-		iterator _start;
-		iterator _finish;
-		iterator _end_of_;
+		iterator _start=nullptr;
+		iterator _finish=nullptr;
+		iterator _end=nullptr;
 	};
-
 	template<class T>
-	vector<T>::vector()
-		:_start(nullptr)
-		,_finish(nullptr)
-		,_end_of_(nullptr)
+	void vector<T>::push_back(const T& val)
 	{
+		if (_finish == _end)
+		{
+			size_t newcapacity = capacity() == 0 ? 4 : 2 * capacity();
+			reserve(newcapacity);
+		}
+		*_finish = val;
+		++_finish;
 	}
+	template<class T>
+	void vector<T>::pop_back()
+	{
+		assert(size());
+		--_finish;
+	}
+	template<class T>
+	void vector<T>::insert(iterator pos,const T& val)
+	{
+		assert(_start <= pos);
+		assert(_finish>= pos);
+		if (_finish == _end)
+		{
+			size_t n = pos - _start;
+			size_t newcapacity = capacity() == 0 ? 4 : 2 * capacity();
+			reserve(newcapacity);
+			pos = _start + n;
+		}
+		_finish++;
+		iterator end = _finish-1;
+		while (end != pos)
+		{
+			*end = *(end - 1);
+			end--;
+		}
+		*pos = val;
+	}
+	template<class T>
+	void vector<T>::reserve(const size_t n)
+	{
+		if (n > capacity())
+		{
+			size_t oldn = size();
+			T* temp = new T[n];
+			if(_start!=nullptr)
+			{
+				size_t end = size();
+				while (end > 0)
+				{
+					temp[end-1] = _start[end-1];
+					end--;
+				}
+				delete[] _start;
+			}
+			_start = temp;
+			_finish = _start + oldn;
+			_end = _start + n;
+		}
+	}
+	template<class T>
+	void vector<T>::resize(const size_t n, T val)
+	{
+		if (n > size())
+		{
+			reserve(n);
+			for (size_t i = size(); i < n; i++)
+			{
+				_start[i] = val;
+				_finish++;
+			}
+		}
+		else
+		{
+			_finish = _start+ n;
+		}
+	}
+	template<class T>
+	T& vector<T>::operator[](size_t pos)
+	{
+		assert(pos < size());
+		return *(_start + pos);
+	}
+	template<class T>
+	const T& vector<T>::operator[](size_t pos)const
+	{
+		assert(pos < size());
+		return *(_start + pos);
+	}
+	template<class T>
+	vector<T>& vector<T>::operator=( vector<T> v)
+	{
+		swap(v);
+		return *this;
+	}
+
+
 }
 
 #endif // !M_VECTOR
