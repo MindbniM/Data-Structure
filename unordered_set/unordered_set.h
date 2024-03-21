2353cb6e0383f9e327c125410d8b991c464de57c
MindbniM <118365358+MindbniM@users.noreply.github.com>
2024-03-21 20:45:40 +0800

    Add files via upload

--- unordered_set/unordered_set.h
+++ unordered_set/unordered_set.h
@@ -1,17 +1,32 @@
 #pragma once
-template<class T>
-struct koft
-{
-	T operator()(const T& key)
-	{
-		return key;
-	}
-};
-template<class K,class T,class koft=koft<T>,class Hash=Hashfun<T>>
+template<class K,class Hash=Hashfun<K>>
 class unordered_set
 {
+	struct koft
+	{
+		K operator()(const K& key)
+		{
+			return key;
+		}
+	};
 public:
-	
+	typedef typename hash_bucket<K, K, koft, Hash>::iterator iterator;
+	iterator begin()
+	{
+		return hash.begin();
+	}
+	iterator end()
+	{
+		return hash.end();
+	}
+	std::pair<iterator, bool> insert(const K& data)
+	{
+		return hash.insert(data);
+	}
+	iterator erase(const K& key)
+	{
+		return hash.erase(key);
+	}
 private:
-	hash_bucket<K, T, koft, Hash> hash;
+	hash_bucket<K, K, koft, Hash> hash;
 };
