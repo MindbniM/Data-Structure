cccaba214842d80783ed4d5bb53fd32b1134cebe
MindbniM <118365358+MindbniM@users.noreply.github.com>
2024-04-26 19:56:30 +0800

    Add files via upload

--- list/list.h
+++ list/list.h
@@ -1,4 +1,5 @@
 #pragma once
+#include"Reverse_iterator.h"
 namespace M
 {
 	template<class T>
@@ -11,6 +12,12 @@ namespace M
 			:_prev(nullptr)
 			,_next(nullptr)
 			,_val(val)
+		{
+		}
+		ListNode(T&& val)
+			:_prev(nullptr)
+			,_next(nullptr)
+			,_val(std::forward<T>(val))
 		{
 		}
 	};
@@ -43,7 +50,7 @@ namespace M
 		{
 			return _node->_val;
 		}
-		bool operator!=(const self& li) const
+		bool operator!=( const self& li) const
 		{
 			return _node != li._node;
 		}
@@ -64,16 +71,12 @@ namespace M
 	public:
 		typedef List_iterator<T,T&,T*> iterator;
 		typedef List_iterator<T,const T&,const T*> const_iterator;
+		typedef Reverse_iterator<iterator, T&, T*> reverse_iterator;
+		typedef Reverse_iterator<const_iterator,const T&, const T*> const_reverse_iterator;
 
 		list()
 		{
 			empty_init();
-		}
-		void empty_init()
-		{
-			_head = new ListNode<T>();
-			_head->_next = _head;
-			_head->_prev = _head;
 		}
 		list(const list<T>& li)
 		{
@@ -81,6 +84,18 @@ namespace M
 			for (auto& l : li)
 			{
 				push_back(l);
+			}
+		}
+		list(list<T>&& li)
+		{
+			swap(li);
+		}
+		list(std::initializer_list<T> li)
+		{
+			empty_init();
+			for (auto& i : li)
+			{
+				push_back(i);
 			}
 		}
 		~list()
@@ -105,11 +120,39 @@ namespace M
 		{
 			erase(begin());
 		}
+		template<class ... Args>
+		iterator emplace(iterator it, Args&&... args)
+		{
+			Node* prev = it._node->_prev;
+			Node* node = it._node;
+			Node* newnode = new Node(std::forward<Args>(args)...);
+			prev->_next = newnode;
+			newnode->_prev = prev;
+			newnode->_next = node;
+			node->_prev = newnode;
+			return newnode;
+		}
+		template<class ...Args>
+		void emplace_back(Args&&...args)
+		{
+			emplace(end(), args...);
+		}
 		iterator insert(iterator it,const T& val)
 		{
 			Node* prev = it._node->_prev;
 			Node* node = it._node;
 			Node* newnode = new Node(val);
+			prev->_next = newnode;
+			newnode->_prev = prev;
+			newnode->_next = node;
+			node->_prev = newnode;
+			return newnode;
+		}
+		iterator insert(iterator it,T&& val)
+		{
+			Node* prev = it._node->_prev;
+			Node* node = it._node;
+			Node* newnode = new Node(std::forward<T>(val));
 			prev->_next = newnode;
 			newnode->_prev = prev;
 			newnode->_next = node;
@@ -142,6 +185,22 @@ namespace M
 		{
 			return _head;
 		}
+		reverse_iterator rbegin()
+		{
+			return reverse_iterator(end());
+		}
+		reverse_iterator rend()
+		{
+			return reverse_iterator(begin());
+		}
+		const_reverse_iterator crbegin() const
+		{
+			return const_reverse_iterator(end());
+		}
+		const_reverse_iterator crend() const
+		{
+			return const_reverse_iterator(begin());
+		}
 		void clear()
 		{
 			iterator b = begin();
@@ -161,12 +220,44 @@ namespace M
 				push_back(val);
 			}
 		}
-		list& operator=(list li)
+		bool empty()
+		{
+			return _head->_next == _head;
+		}
+		int size()
+		{
+			iterator it = begin();
+			int n = 0;
+			while (it != end())
+			{
+				++it;
+				n++;
+			}
+			return n;
+		}
+		list& operator=(const list& li)
+		{
+			empty_init();
+			for (auto&i : li)
+			{
+				push_back(i);
+			}
+		}
+		list& operator=(list&& li)
 		{
 			swap(li);
 			return *this;
 		}
 	private:
 		Node* _head;
+
+
+
+		void empty_init()
+		{
+			_head = new ListNode<T>();
+			_head->_next = _head;
+			_head->_prev = _head;
+		}
 	};
 }
