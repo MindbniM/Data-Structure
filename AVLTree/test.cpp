3a332c6388fa9d686d0a72a392ec10b4053d39df
MindbniM <118365358+MindbniM@users.noreply.github.com>
2024-08-24 21:54:49 +0800

    Add files via upload

--- AVLTree/test.cpp
+++ AVLTree/test.cpp
@@ -7,29 +7,26 @@ using namespace std;
 #include"AVL.h"
 int main()
 {
-    const int N = 100000;
+    //const int N = 100000;
     AVLTree<int, int> node;
-    srand(time(nullptr));
-    /*vector<int> v{ 4, 2, 6, 1, 3, 5, 15, 7, 16, 14 };
+    //srand(time(nullptr));
+    vector<int> v{ 4, 2, 6, 1, 3, 5, 15, 7, 16, 14 };
     for (int i = 0;i < v.size(); i++)
     {
         node.insert(make_pair(v[i],1));
-        node.order();
-
-
     }
-    cout<< node.isAVLTree();*/
-    for (int i = 0; i < N; i++)
+    node.inorder();
+    /*for (int i = 0; i < N; i++)
     {
         int n = rand();
         node.insert(make_pair(n,1));
-    }
+    }*/
     /*node.insert(make_pair(6, 1));
     node.insert(make_pair(2, 1));
     node.insert(make_pair(3, 1));
     node.insert(make_pair(0, 1));
     node.inorder();
     cout << endl;*/
-    cout << node.isAVLTree();
+    //cout << node.isAVLTree();
     return 0;
 }
